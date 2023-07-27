d784e6c6afd9b0b040f4c93d6c8fc15084ffec69
yra <ymz.ncnk@outlook.com>
2023-07-27 14:33:08 +0300

    Complete the metadata example.

--- metadata/main.go
+++ metadata/main.go
@@ -15,28 +15,39 @@ const (
 
 // Shows how to use data type metadata.
 func main() {
-	bs := makeSlice()
+	wantFoo, bs := makeFooSlice()
+	wantBar, _ := makeBarSlice()
 	// If we don't know a concrete type of the encoded data, metadata will help
 	// us.
-	dt, _, err := UnmarshalDataType(bs)
+	dt, n, err := UnmarshalDataType(bs)
 	assert.EqualError(err, nil)
 
 	switch dt {
 	case FooType:
 		// Unmarhsal Foo
+		foo, _, err := UnmarshalFoo(bs[n:])
+		assert.EqualError(err, nil)
+		assert.EqualDeep(foo, wantFoo)
 	case BarType:
 		// Unmarshal Bar
+		bar, _, err := UnmarshalBar(bs[n:])
+		assert.EqualError(err, nil)
+		assert.EqualDeep(bar, wantBar)
 	default:
 		panic("unexpected DataType")
 	}
 }
 
-func makeSlice() (bs []byte) {
-	var (
-		foo  = Foo{a: 1, b: true}
-		size = SizeMetaFoo(foo)
-	)
-	bs = make([]byte, size)
+func makeFooSlice() (foo Foo, bs []byte) {
+	foo = Foo{a: 1, b: true}
+	bs = make([]byte, SizeMetaFoo(foo))
 	MarshalMetaFoo(foo, bs)
+	return
+}
+
+func makeBarSlice() (bar Bar, bs []byte) {
+	bar = Bar{a: "hello world", b: true}
+	bs = make([]byte, SizeMetaBar(bar))
+	MarshalMetaBar(bar, bs)
 	return
 }
